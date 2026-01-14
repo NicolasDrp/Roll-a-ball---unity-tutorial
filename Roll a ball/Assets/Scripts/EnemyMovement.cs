85ee4a4ee8e44f9ca5310881b92b519d0f51e08b
NicolasDrp <drapier.nicolas07@gmail.com>
2026-01-14 23:03:45 +0100

    feat: add some static obstacles

---
+++ Roll a ball/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public Transform player; // Reference to the player's transform
+
+    private NavMeshAgent navMeshAgent; // Reference to the NavMeshAgent component
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+        {
+            navMeshAgent.SetDestination(player.position);
+        }
+    }
+}
